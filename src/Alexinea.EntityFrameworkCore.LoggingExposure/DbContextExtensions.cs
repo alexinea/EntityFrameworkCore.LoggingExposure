b5dcec42443084009263da46876cf54d6bb5d000
alexinea <forerunner@outlook.com>
2018-02-01 09:20:12 +0800

    cleanup and new logo

--- src/Alexinea.EntityFrameworkCore.LoggingExposure/DbContextExtensions.cs
+++ src/Alexinea.EntityFrameworkCore.LoggingExposure/DbContextExtensions.cs
@@ -8,30 +8,28 @@ using Microsoft.Extensions.Logging;
 namespace Alexinea.EntityFrameworkCore.LoggingExposure {
     public static class DbContextExtensions {
         public static void ConfigureLogging(this DbContext db, Action<string> logger, Func<string, LogLevel, bool> filter) {
-            var serviceProvider = db.GetInfrastructure<IServiceProvider>();
-            var loggerFactory = (ILoggerFactory) serviceProvider.GetService(typeof(ILoggerFactory));
+            var loggerFactory = (ILoggerFactory) db.GetInfrastructure().GetService(typeof(ILoggerFactory));
 
             LogProvider.CreateOrModifyLoggerForDbContext(db.GetType(), loggerFactory, logger, filter);
         }
 
         public static void ConfigureLogging(this DbContext db, Action<string> logger, LoggingCategories categories = LoggingCategories.All) {
-            var serviceProvider = db.GetInfrastructure<IServiceProvider>();
-            var loggerFactory = (LoggerFactory) serviceProvider.GetService(typeof(ILoggerFactory));
+            var loggerFactory = (LoggerFactory) db.GetInfrastructure().GetService(typeof(ILoggerFactory));
 
-            if (categories == LoggingCategories.Sql) {
-                var sqlCategories = new List<string> {
-                    DbLoggerCategory.Database.Command.Name,
-                    DbLoggerCategory.Query.Name,
-                    DbLoggerCategory.Update.Name
-                };
-                LogProvider.CreateOrModifyLoggerForDbContext(db.GetType(),
-                    loggerFactory,
-                    logger,
-                    (c, l) => sqlCategories.Contains(c));
-            } else if (categories == LoggingCategories.All) {
-                LogProvider.CreateOrModifyLoggerForDbContext(db.GetType(),
-                    loggerFactory, logger,
-                    (c, l) => true);
+            switch (categories) {
+                case LoggingCategories.Sql: {
+                    var sqlCategories = new List<string> {
+                        DbLoggerCategory.Database.Command.Name,
+                        DbLoggerCategory.Query.Name,
+                        DbLoggerCategory.Update.Name
+                    };
+                    LogProvider.CreateOrModifyLoggerForDbContext(db.GetType(), loggerFactory, logger, (c, l) => sqlCategories.Contains(c));
+                    break;
+                }
+                case LoggingCategories.All: {
+                    LogProvider.CreateOrModifyLoggerForDbContext(db.GetType(), loggerFactory, logger, (c, l) => true);
+                    break;
+                }
             }
         }
     }
